0ce08959197ea6edcec4e76dffe3788b508af078
mohamed-Fathy1 <60184431+mohamed-Fathy1@users.noreply.github.com>
2023-06-28 05:56:14 +0300

    Create 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function
+ * @array: input
+ * @size: input
+ */
+void bubble_sort(int *array, size_t size)
+{
+    int i, j;
+
+    if (array == NULL || size < 2)
+		    return;
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                char temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
+}
