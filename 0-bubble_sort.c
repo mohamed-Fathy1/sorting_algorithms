9b374d6c8a4d264d6d64e922f67b2424358ffa9e
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-29 16:14:39 +0300

    Add task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,14 +12,14 @@ void bubble_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		isSorted = 1;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				char temp = array[j];
+				int temp = array[j];
 
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
