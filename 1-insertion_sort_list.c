5369bd5e3405dd6fa1ec32e0f25ed9cdfaf94179
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-07-01 18:05:20 +0300

    Add insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+
+/**
+ * insertion_sort_list - function
+ * @list: input
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node = (*list)->next;
+	listint_t *i, *j;
+
+	for (i = node; i && i->prev; i = i->next)
+	{
+		listint_t *cur = i;
+
+		for (j = i; j && j->prev && (i->prev->n > i->n); j = j->prev)
+		{
+			listint_t *next = cur->next;
+			listint_t *prev = cur->prev;
+
+			prev->next = next;
+			if (next)
+				next->prev = prev;
+
+			if (prev->prev)
+				prev->prev->next = cur;
+			else
+				*list = cur;
+
+			if (prev)
+				cur->prev = prev->prev;
+			prev->prev = cur;
+
+			cur->next = prev;
+			print_list(*list);
+		}
+	}
+}
